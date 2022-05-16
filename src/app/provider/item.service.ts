e6dce6a5e41e753a9ec891928b03074f754b086a
Pin-Zhen Chen <11gives1111@gmail.com>
2022-05-16 13:11:36 +0200

    update API

--- src/app/provider/item.service.ts
+++ src/app/provider/item.service.ts
@@ -7,7 +7,8 @@ import { Globals } from "../globals";
 @Injectable() 
 export class ItemService {
    // private gamifyUrl: string = 'http:///127.0.0.1:6789/api/constant/basic/30/14/inf/0/7000/0/60/t/2/10/tree/u123/getTasksForToday'
-    private gamifyUrl: string = 'https://aqueous-hollows-34193.herokuapp.com/api/smdp/mdp/30/14/inf/0/inf/0/inf/false/0/max/0.999999/0.1/2/1.39/0.0001/0.01/tree/__test__/getTasksForToday'
+   // private gamifyUrl: string = 'https://aqueous-hollows-34193.herokuapp.com/api/smdp/mdp/30/14/inf/0/inf/0/inf/false/0/max/0.999999/0.1/2/1.39/0.0001/0.01/tree/__test__/getTasksForToday'
+    private gamifyUrl: string = "https://yellow-tree.herokuapp.com/api/greedy/mdp/45/14/inf/0/inf/0/inf/0/no_scaling/true/0/tree/test/getTasksForToday"
     constructor(private http: HttpClient) {}
 
     public goalname_map = {};//add a goalname map

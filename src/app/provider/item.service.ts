b16091a7f88ecdab4e4bfdcbd8c9468f1332d58d
Konrad Fritsche <konrad.frit@gmail.com>
2022-05-31 19:08:10 +0200

    refactor | simplify item service and add attributes holding states for goals and optimized goals

--- src/app/provider/item.service.ts
+++ src/app/provider/item.service.ts
@@ -1,126 +1,163 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-import { Observable, throwError } from 'rxjs';
-import { Globals } from "../globals";
+import { Observable, ReplaySubject } from 'rxjs';
+import { first } from 'rxjs/operators';
 
-@Injectable() 
+import { Globals } from "../globals";
+import { Goal, outputItem } from "../interfaces/item";
+
+@Injectable()
 export class ItemService {
-    // private gamifyUrl: string = 'http:///127.0.0.1:6789/api/constant/basic/30/14/inf/0/7000/0/60/t/2/10/tree/u123/getTasksForToday'
-    private gamifyUrl: string = 'https://yellow-tree.herokuapp.com/api/greedy/mdp/45/14/inf/0/inf/0/inf/0/no_scaling/true/0/tree/test/getTasksForToday'
-    constructor(private http: HttpClient) {}
 
     public goalname_map = {};//add a goalname map
-    public goal_map ={}; //add a goalmap for all info
+    public goal_map = {}; //add a goalmap for all info
+
+    // private gamifyUrl: string = 'http:///127.0.0.1:6789/api/constant/basic/30/14/inf/0/7000/0/60/t/2/10/tree/u123/getTasksForToday'
+    private gamifyUrl: string = 'https://yellow-tree.herokuapp.com/api/greedy/mdp/45/14/inf/0/inf/0/inf/0/no_scaling/true/0/tree/test/getTasksForToday'
+    private userKey: string;
+    private currentIntentions: string;
+
+    private goals$ = new ReplaySubject<Goal[]>();
+    private optimizedGoals$ = new ReplaySubject<outputItem[]>();
+
+    constructor(private http: HttpClient) {
+        this.setGoals(Globals.goalList);
+     }
+
 
     makeProject() {
-        console.log("In makeProject");
         let project = [];
-        for (let i = 1; i < Globals.goalList.length+1; i++){
-            console.log(i);
+        for (let i = 1; i < Globals.goalList.length + 1; i++) {
             var node = {
-                id: "g"+ i,
-                nm: "#CG" + i + "_" + Globals.goalList[i-1].name + " ==" + Globals.goalList[i-1].value + " ~~" + Globals.goalList[i-1].time_est + "h DUE:" + Globals.goalList[i-1].deadline,
+                id: "g" + i,
+                nm: "#CG" + i + "_" + Globals.goalList[i - 1].name + " ==" + Globals.goalList[i - 1].value + " ~~" + Globals.goalList[i - 1].time_est + "h DUE:" + Globals.goalList[i - 1].deadline,
                 lm: 0,
                 ch: null,
                 parentId: "None"
-              }
-            this.goalname_map[i-1]=Globals.goalList[i-1].name;//update a goalname map
-            this.goal_map[i-1]=Globals.goalList[i-1].name + "/ Value: "+ Globals.goalList[i-1].value + "/ Time Estimation: "+ Globals.goalList[i-1].time_est + "h/ Deadline: " + Globals.goalList[i-1].deadline; //update a goalmap
-            if ('num_children' in Globals.goalList[i-1] && Globals.goalList[i-1].num_children > 0){
+            }
+            this.goalname_map[i - 1] = Globals.goalList[i - 1].name;//update a goalname map
+            this.goal_map[i - 1] = Globals.goalList[i - 1].name + "/ Value: " + Globals.goalList[i - 1].value + "/ Time Estimation: " + Globals.goalList[i - 1].time_est + "h/ Deadline: " + Globals.goalList[i - 1].deadline; //update a goalmap
+            if ('num_children' in Globals.goalList[i - 1] && Globals.goalList[i - 1].num_children > 0) {
                 node.ch = [];
-                for (let j = 1; j < Globals.goalList[i-1].num_children+1; j++){
-                    let td = Globals.goalList[i-1].tasks[j-1].today ? " #today": " #future";
+                for (let j = 1; j < Globals.goalList[i - 1].num_children + 1; j++) {
+                    let td = Globals.goalList[i - 1].tasks[j - 1].today ? " #today" : " #future";
                     var task = {
-                        id: "g"+ i + "-t" + j,
-                        nm: Globals.goalList[i-1].tasks[j-1].name + " ~~" + Globals.goalList[i-1].tasks[j-1].time_est + "h" + td,
+                        id: "g" + i + "-t" + j,
+                        nm: Globals.goalList[i - 1].tasks[j - 1].name + " ~~" + Globals.goalList[i - 1].tasks[j - 1].time_est + "h" + td,
                         lm: 0,
                         parentId: node.id
-                      }
+                    }
                     node.ch.push(task);
                 }
             }
-            console.log("NODE", node);
-            project.push(node);
-            console.log("Project");
-            console.log(project);
-            console.log("String Project");
-            JSON.stringify(project);
-            console.log(project);
 
-            console.log("print map of goal name__: ", this.goalname_map) //
-            console.log("print map of goalmap with goal info: ", this.goal_map)
+            project.push(node);
+            JSON.stringify(project);
         }
         return project;
     }
     //need to set GoalName_map ?
-    setGoalName_map(goalname_map){
-    this.goalname_map = goalname_map;
+    public setGoalName_map(goalname_map) {
+        this.goalname_map = goalname_map;
     }
-    setGoal_map(goal_map){
+    public setGoal_map(goal_map) {
         this.goal_map = goal_map;
-        }
-    make_typical_hours() {
-        let list = [];
-        let obj = {
-            id: "_",
-            nm: "#HOURS_TYPICAL ==10000",
-            lm: 0
-        };
-        list.push(obj);
-        JSON.stringify(list)
-        console.log('Typ Hours', list);
-        return list;
     }
 
-    make_today_hours() {
-        let list = [];
-        let obj = {
-            id: "_",
-            nm: "#HOURS_TODAY ==10000",
-            lm: 0
-        };
-        list.push(obj);
-        JSON.stringify(list)
-        console.log('Today Hours', list);
-        return list;
+    public setOptimizedGoals(optimizedGoals: outputItem[]) {
+        this.optimizedGoals$.next(optimizedGoals);
     }
-    getOptimalList(): Observable<any> {
-        let list: {} = {}
 
-        // Globals.taskList.forEach(li => {
-        //     list[li.name.toLowerCase()] = li.amount
-        // })
+    public setGoals(goals: Goal[]) {
+        this.goals$.next(goals);
+    }
 
+    public getGoals(): Observable<Goal[]> {
+        return this.goals$
+        .pipe(
+            first()
+        );
+    }
+
+    public getOptimizedGoals(): Observable<outputItem[]> {
+        return this.optimizedGoals$
+            .pipe(
+                first()
+            );
+    }
+
+    public listenToOptimizedGoals(): Observable<outputItem[]> {
+        return this.optimizedGoals$.asObservable();
+    }
+
+    public requestOptimalTodoList(): Observable<any> {
+        const options = this.createRequestOptions();
+        const body = this.createRequestBody();
+
+        return this.http.post(this.gamifyUrl, body, options);
+    }
+
+    private makeUserKey() {
+        return this.userKey || '__test__';
+    }
+
+    private makeCurrentIntentions() {
+        return this.currentIntentions || [];
+    }
+
+    private makeTypicalHours() {
+        const value = "#HOURS_TYPICAL ==10000";
+
+        return this.makeHoursNode(value);
+    }
+
+    private makeTodayHours() {
+        const value = "#HOURS_TODAY ==10000";
+
+        return this.makeHoursNode(value);
+    }
+
+    private makeHoursNode(value: string) {
+        const hourNode = [
+            {
+                id: "_",
+                nm: value,
+                lm: 0
+            }
+        ];
+
+        return hourNode;
+    }
+
+    private createRequestOptions() {
         const headers = new HttpHeaders({
             'Content-Type': 'application/json',
         })
 
-        const options = {
-            headers,
-        }
-        console.log("Making List to be sent to API");
-        let project = this.makeProject();
-        console.log("Returned from makeProject");
-        let typical_hours = this.make_typical_hours();
-        let today_hours = this.make_today_hours();
-        console.log("Make Today Hours");
-        console.log(today_hours);
-        let body = {
-            currentIntentionsList: [],
-            projects: project,
+        return { headers };
+    }
+
+    private createRequestBody() {
+        const projects = this.makeProject();
+        const typicalHours = this.makeTypicalHours();
+        const todayHours = this.makeTodayHours();
+        const currentIntentions = this.makeCurrentIntentions();
+        const userKey = this.makeUserKey();
+
+        const defaultParameters = {
             timezoneOffsetMinutes: 0,
-            today_hours: today_hours, 
-            typical_hours: typical_hours,
             updated: 0,
-            userkey: '__test__',
-            bias: 683.2111000706025,
-            scale: 1.1,
             time_frame: 480
         };
-        console.log("Body of POST");
-        console.log(body);
-        return this.http.post(this.gamifyUrl, body, options);
 
+        return {
+            ...defaultParameters,
+            currentIntentionsList: currentIntentions,
+            projects: projects,
+            today_hours: todayHours,
+            typical_hours: typicalHours,
+            userkey: userKey
+        };
     }
 }

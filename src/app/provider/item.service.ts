a07e6faf487bbc3eca8d96e8007a6edca96fd1be
Konrad Fritsche <konrad.frit@gmail.com>
2022-06-02 14:18:34 +0200

    web component | refactor item service to use internal observables instead of static globals

--- src/app/provider/item.service.ts
+++ src/app/provider/item.service.ts
@@ -2,16 +2,13 @@ import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable, ReplaySubject } from 'rxjs';
-import { first } from 'rxjs/operators';
+import { first, map, switchMap } from 'rxjs/operators';
 
 import { Globals } from "../globals";
 import { Goal, outputItem } from "../interfaces/item";
 
 @Injectable()
 export class ItemService {
-
-    public goalname_map = {};//add a goalname map
-    public goal_map = {}; //add a goalmap for all info
 
     // private gamifyUrl: string = 'http:///127.0.0.1:6789/api/constant/basic/30/14/inf/0/7000/0/60/t/2/10/tree/u123/getTasksForToday'
     private gamifyUrl: string = 'https://yellow-tree.herokuapp.com/api/greedy/mdp/45/14/inf/0/inf/0/inf/0/no_scaling/true/0/tree/test/getTasksForToday'
@@ -26,25 +23,23 @@ export class ItemService {
      }
 
 
-    makeProject() {
+    makeProject(goals: Goal[]) {
         let project = [];
-        for (let i = 1; i < Globals.goalList.length + 1; i++) {
+        for (let i = 1; i < goals.length + 1; i++) {
             var node = {
                 id: "g" + i,
-                nm: "#CG" + i + "_" + Globals.goalList[i - 1].name + " ==" + Globals.goalList[i - 1].value + " ~~" + Globals.goalList[i - 1].time_est + "h DUE:" + Globals.goalList[i - 1].deadline,
+                nm: "#CG" + i + "_" + goals[i - 1].name + " ==" + goals[i - 1].value + " ~~" + goals[i - 1].time_est + "h DUE:" + goals[i - 1].deadline,
                 lm: 0,
                 ch: null,
                 parentId: "None"
             }
-            this.goalname_map[i - 1] = Globals.goalList[i - 1].name;//update a goalname map
-            this.goal_map[i - 1] = Globals.goalList[i - 1].name + "/ Value: " + Globals.goalList[i - 1].value + "/ Time Estimation: " + Globals.goalList[i - 1].time_est + "h/ Deadline: " + Globals.goalList[i - 1].deadline; //update a goalmap
-            if ('num_children' in Globals.goalList[i - 1] && Globals.goalList[i - 1].num_children > 0) {
+            if ('num_children' in goals[i - 1] && goals[i - 1].num_children > 0) {
                 node.ch = [];
-                for (let j = 1; j < Globals.goalList[i - 1].num_children + 1; j++) {
-                    let td = Globals.goalList[i - 1].tasks[j - 1].today ? " #today" : " #future";
+                for (let j = 1; j < goals[i - 1].num_children + 1; j++) {
+                    let td = goals[i - 1].tasks[j - 1].today ? " #today" : " #future";
                     var task = {
                         id: "g" + i + "-t" + j,
-                        nm: Globals.goalList[i - 1].tasks[j - 1].name + " ~~" + Globals.goalList[i - 1].tasks[j - 1].time_est + "h" + td,
+                        nm: goals[i - 1].tasks[j - 1].name + " ~~" + goals[i - 1].tasks[j - 1].time_est + "h" + td,
                         lm: 0,
                         parentId: node.id
                     }
@@ -57,14 +52,6 @@ export class ItemService {
         }
         return project;
     }
-    //need to set GoalName_map ?
-    public setGoalName_map(goalname_map) {
-        this.goalname_map = goalname_map;
-    }
-    public setGoal_map(goal_map) {
-        this.goal_map = goal_map;
-    }
-
     public setOptimizedGoals(optimizedGoals: outputItem[]) {
         this.optimizedGoals$.next(optimizedGoals);
     }
@@ -96,10 +83,19 @@ export class ItemService {
     }
 
     public requestOptimalTodoList(): Observable<any> {
-        const options = this.createRequestOptions();
-        const body = this.createRequestBody();
+        return this.getGoals()
+            .pipe(
+                map(goals => this.makeTodoListRequest(goals)),
+                switchMap(request => this.http.post(request.url, request.body, request.options))
+            );
+    }
 
-        return this.http.post(this.gamifyUrl, body, options);
+    private makeTodoListRequest(goals: Goal[]) {
+        return {
+           options: this.createRequestOptions(),
+           body: this.createRequestBody(goals),
+           url: this.gamifyUrl
+        };
     }
 
     private makeUserKey() {
@@ -142,8 +138,8 @@ export class ItemService {
         return { headers };
     }
 
-    private createRequestBody() {
-        const projects = this.makeProject();
+    private createRequestBody(goals: Goal[]) {
+        const projects = this.makeProject(goals);
         const typicalHours = this.makeTypicalHours();
         const todayHours = this.makeTodayHours();
         const currentIntentions = this.makeCurrentIntentions();

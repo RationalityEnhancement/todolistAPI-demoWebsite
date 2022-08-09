f0c03e797103b295b17d8665c879933cb6c92dcb
Konrad Fritsche <konrad.frit@gmail.com>
2022-08-09 12:59:37 +0200

    make tasks completeable

--- src/app/components/task-preview/task-preview.component.ts
+++ src/app/components/task-preview/task-preview.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { faCheck, faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { faCheck, faArrowAltCircleLeft, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import { Item } from 'src/app/interfaces/item';
 
 @Component({
@@ -12,30 +12,37 @@ export class TaskPreviewComponent implements OnInit {
   @Input() public task: Item;
 
   @Output() public deleteSubmit = new EventEmitter();
+  @Output() public completeSubmit = new EventEmitter<Item>();
 
   public isOverdue: boolean;
 
   public completeIcon = faCheck;
   public deleteIcon = faTrashAlt;
-  public editIcon = faEdit
+  public undoIcon = faArrowAltCircleLeft;
 
 
   constructor() { }
 
   ngOnInit(): void {
     this.setOverdueStatus();
-   }
+  }
 
-   public delete(): void {
+  public deleteTask(): void {
     this.deleteSubmit.emit();
-   }
+  }
 
-   private setOverdueStatus(): void {
+  public toggleComplete(): void {
+    const task = { ...this.task, completed: !this.task.completed };
+
+    this.completeSubmit.emit(task);
+  }
+
+  private setOverdueStatus(): void {
     if (this.task.deadline) {
       const deadlineDate = new Date(this.task.deadline);
       const todayDate = new Date(new Date().toISOString().substring(0, 10));
 
       this.isOverdue = deadlineDate < todayDate;
     }
-   }
+  }
 }
